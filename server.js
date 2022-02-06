038f06e7f9179464a51bc99e7c38a935cb43a69a
dgsmrn <dgsmrn@outlook.com>
2022-02-06 09:31:00 -0300

    nodeejs

--- server.js
+++ server.js
@@ -7,7 +7,18 @@ app.set("view engine", "ejs");
 
 //get() cria uma rota na raiz do site e executa o index
 app.get("/", function (req, res) {
-  res.render("pages/index");
+  const items = [
+    { title: "D", message: "esenvolver aplicações/serviços de forma fácil" },
+    { title: "E", message: "JS usa Javascript para renderizar HTML"},
+    { title: "M", message: "uito fácil de usar"},
+    { title: "A", message: "morzinho"},
+    { title: "I", message: "nstall ejs"},
+    { title: "S", message: "intaxe simples"},
+  ];
+
+  const subtitle = "Uma linguagem de modelagem para criação de páginas HTML utilizando JS"
+
+  res.render("pages/index", {qualitys: items, subtitle: subtitle} ); //renderiza a página passando um objeto que recebe um array
 });
 
 //get() cria uma rota para executar a página sobre que representa o arquivo about.ejs
@@ -15,6 +26,6 @@ app.get("/sobre", function (req, res) {
   res.render("pages/about");
 });
 
-//funcao do express que fica ouvindo a porta 8080 -- http://localhost:8080/
-app.listen(8080)
-console.log("Sevidor Funcionando")
+//funcao do express que fica ouvindo a porta 8080 -- http://localhost:8081/
+app.listen(8081);
+console.log("Sevidor Funcionando");
